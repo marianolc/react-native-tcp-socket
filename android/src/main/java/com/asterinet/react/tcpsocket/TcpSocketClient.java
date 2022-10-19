2ed1b35406caa1e4f3853ab65d4051c24ca8d0c3
mlc <mcano@dlocorp.com>
2022-10-19 12:35:54 -0600

    merged. 6.0.2
    Signed-off-by: mlc <mcano@dlocorp.com>

--- android/src/main/java/com/asterinet/react/tcpsocket/TcpSocketClient.java
+++ android/src/main/java/com/asterinet/react/tcpsocket/TcpSocketClient.java
@@ -199,6 +199,13 @@ class TcpSocketClient extends TcpSocket {
                 if (receiverListener != null && !socket.isClosed() && !clientSocket.closed) {
                     receiverListener.onError(socketId, ioe);
                 }
+            } catch (Exception exception) {
+                try {
+                    receiverListener.onError(socketId, exception);
+                    // receiverListener.onClose(getId(), null);
+                } catch (Exception ignored) {
+                    // nothing to do!
+                }
             }
         }
 
